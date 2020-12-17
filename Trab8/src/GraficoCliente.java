e0a4c3f85d547e4dcb8b7575eb25398bf774361c
João Lucas Nascimento <johnascimentom@hotmail.com>
2020-12-16 23:53:15 -0300

    Interfaces

---
+++ Trab8/src/GraficoCliente.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GraficoCliente {
+        private JTextField textField1;
+        private JPanel panel1;
+        private JTextField textField2;
+        private JTextField textField4;
+        private JTextField textField5;
+        private JTextField textField3;
+}
